3646f41c1be1bf216651314056b7419c5908e698
Vinicius <vinicius-omf@hotmail.com>
2018-04-18 01:08:27 -0300

    first commit

---
+++ js/personagem.js
@@ -0,0 +1,49 @@
+document.addEventListener("keydown", keyDownTextField, false);
+
+function keyDownTextField(e) {
+
+	var keyCode = e.keyCode;
+	if(keyCode==39) {
+		document.getElementById('personagem').className = '';
+		document.getElementById('personagem').className = 'andandoDireita';
+		moverProLadoDireito();
+
+	} 
+	else if(keyCode == 37){
+		document.getElementById('personagem').className = '';
+		document.getElementById('personagem').className = 'andandoEsquerda';
+		moverProLadoEsquerdo()
+
+	}
+	else if(keyCode == 38){
+		document.getElementById('personagem').className = '';
+		document.getElementById('personagem').className = 'parado';
+
+	}
+}
+
+
+
+function moverProLadoDireito() {
+	let personagem = document.getElementById("personagem");   
+	let pos = personagem.getBoundingClientRect().left;
+	pos=pos+50;
+	$("#personagem").animate({left: pos}, {duration: 500, complete:function(){
+			document.getElementById('personagem').className = '';
+			document.getElementById('personagem').className = 'parado';
+	}});
+	personagem.style.left = pos + 'px'; 
+}
+function moverProLadoEsquerdo() {
+	let personagem = document.getElementById("personagem");
+	let pos = personagem.getBoundingClientRect().left;
+	if(pos>10){
+		pos=pos-50;
+		$("#personagem").animate({left: pos}, {duration: 500, complete:function(){
+			document.getElementById('personagem').className = '';
+			document.getElementById('personagem').className = 'parado';
+		}});
+		personagem.style.left = pos + 'px'; 
+	}   
+	
+}
